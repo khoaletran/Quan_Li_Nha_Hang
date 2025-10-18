af3751b5d61f3a79f5bc8b59fdb72491a8687959
Trần Lê Khoa <86742521+khoaletran@users.noreply.github.com>
2025-10-18 21:14:30 +0700

    ui Checkout và sửa lại sidebar

---
+++ src/main/java/ui/controllers/CheckoutController.java
@@ -0,0 +1,11 @@
+package ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
+
+public class CheckoutController {
+
+    @FXML
+    private ToggleGroup paymentGroup;
+
+}
