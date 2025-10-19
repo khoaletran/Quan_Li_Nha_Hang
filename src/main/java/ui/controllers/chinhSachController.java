99c365c15020c739720f49444f879e7aba1749f6
Trần Lê Khoa <86742521+khoaletran@users.noreply.github.com>
2025-10-19 22:16:58 +0700

    ui ChinhSach

---
+++ src/main/java/ui/controllers/chinhSachController.java
@@ -0,0 +1,13 @@
+package ui.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+
+public class chinhSachController {
+
+    @FXML private ToggleGroup groupLoai;
+    @FXML private ToggleGroup groupCoc;
+
+}
