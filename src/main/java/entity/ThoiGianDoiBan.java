423071e03e1ab3dc2b1c646bd5967291102a9c39
QuanDo835 <23700901.quan@student.iuh.edu.vn>
2025-10-25 16:26:39 +0700

    them entity ThoiGianDoiBan

---
+++ src/main/java/entity/ThoiGianDoiBan.java
@@ -0,0 +1,46 @@
+package entity;
+
+public class ThoiGianDoiBan {
+    private String maTGDB;
+    private boolean loaiDatBan;
+    private int thoiGian;
+
+    public String getMaTGDB() {
+        return maTGDB;
+    }
+
+    public void setMaTGDB(String maTGDB) {
+        this.maTGDB = maTGDB;
+    }
+
+    public boolean isLoaiDatBan() {
+        return loaiDatBan;
+    }
+
+    public void setLoaiDatBan(boolean loaiDatBan) {
+        this.loaiDatBan = loaiDatBan;
+    }
+
+    public int getThoiGian() {
+        return thoiGian;
+    }
+
+    public void setThoiGian(int thoiGian) {
+        this.thoiGian = thoiGian;
+    }
+
+    public ThoiGianDoiBan(String maTGDB, boolean loaiDatBan, int thoiGian) {
+        this.maTGDB = maTGDB;
+        this.loaiDatBan = loaiDatBan;
+        this.thoiGian = thoiGian;
+    }
+
+    @Override
+    public String toString() {
+        return "ThoiGianDoiBan{" +
+                "maTGDB='" + maTGDB + '\'' +
+                ", loaiDatBan=" + loaiDatBan +
+                ", thoiGian=" + thoiGian +
+                '}';
+    }
+}
