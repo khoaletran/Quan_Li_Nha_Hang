523f2602595bae928416f75365723d3a5ceed03c
KhanhNg206 <23634741.khanh@student.iuh.edu.vn>
2025-10-26 17:00:30 +0700

    test commit

--- src/main/java/ui/controllers/MainController_NV.java
+++ src/main/java/ui/controllers/MainController_NV.java
@@ -51,10 +51,12 @@ public class MainController_NV {
             Object controller = loader.getController();
             if (controller instanceof ChonMonController chonMonCtrl) {
                 chonMonCtrl.setMainController(this);
+                chonMonCtrl.setNhanVienHien(nhanVien);
             }
 
             if (controller instanceof DatBanController datBanCtrl) {
                 datBanCtrl.setMainController(this);
+                datBanCtrl.setNhanVien(nhanVien);
             }
 
             FadeTransition fadeIn = new FadeTransition(Duration.millis(300), node);
